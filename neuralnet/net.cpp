277a8187dbea5da4a8f0a281598c87ad0a32452d
rishitv26 <varshneyrishit5@gmail.com>
2023-02-25 16:58:40 -0500

    Add project files.

---
+++ neuralnet/net.cpp
@@ -0,0 +1,19 @@
+#include "nnet.h"
+
+class Connection
+{
+private:
+
+public:
+	float weight;
+};
+
+class Node
+{
+private:
+	float number;
+	int bias;
+	
+public:
+	Node();
+};
---
+++ test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "neuralnet/nnet.h"
+
+int main() 
+{
+	return 0;
+}
